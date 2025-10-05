5cde33c3b9022a35c6672a9bafcd5c1075122658
advikmrai <advikmrai@gmail.com>
2025-10-04 20:10:03 -0400

    looking

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,28 @@ import Index from "./pages/index";
 import Schedule from "./pages/Schedule";
 import NotFound from "./pages/NotFound";
 
+const appBase = import.meta.env.VITE_APP_BASE || ''; 
+
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/schedule" element={<Schedule />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        {/* Use the conditional basename here */}
+        <BrowserRouter basename={appBase}> 
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
 
 export default App;
